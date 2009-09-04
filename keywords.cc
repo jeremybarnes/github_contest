f4e96c7c53bd184222f07c5b40202aaf65cd1ac4
Jeremy Barnes <jeremy@barneso.com>
2009-09-04 10:10:33 -0400

    use binary features for keyword svd; big improvement

--- keywords.cc
+++ keywords.cc
@@ -418,8 +418,8 @@ void analyze_keywords(Data & data)
         last_index = index;
 
         for (Cooccurrences::const_iterator
-                 it = repo.keywords_idf.begin(),
-                 end = repo.keywords_idf.end();
+                 it = repo.keywords.begin(),
+                 end = repo.keywords.end();
              it != end;  ++it) {
 
             matrix.rowind[entry_num] = word_to_index[it->with];
@@ -427,7 +427,7 @@ void analyze_keywords(Data & data)
             if (matrix.rowind[entry_num] == -1)
                 throw Exception("invalid entry num");
             
-            matrix.value[entry_num] = it->score;
+            matrix.value[entry_num] = 1.0;//it->score;
             ++entry_num;
         }
     }
